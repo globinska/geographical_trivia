3e0156b722a19b560ca7aa23751688afe3444f09
globinska <eglobinska@gmail.com>
2020-07-06 09:07:34 +0100

    database connection

--- src/pl/globinska/trivia/util/ConnectionProvider.java
+++ src/pl/globinska/trivia/util/ConnectionProvider.java
@@ -1,4 +1,31 @@
 package pl.globinska.trivia.util;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class ConnectionProvider {
+
+    private static DataSource dataSource;
+
+    public static Connection getConnection() throws SQLException{
+        return getDataSource().getConnection();
+    }
+
+    private static DataSource getDataSource() {
+        if (dataSource == null){
+            try {
+                Context initContext = new InitialContext();
+                Context envContext = (Context) initContext.lookup("java:comp/env");
+                DataSource ds = (DataSource) envContext.lookup("jdbc:geographical_trivia");
+                dataSource = ds;
+            } catch (NamingException e){
+                e.printStackTrace();
+            }
+        }
+        return dataSource;
+    }
 }
